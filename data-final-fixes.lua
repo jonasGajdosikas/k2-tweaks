c0cc7e7a7ed705d3d1cb65d9b6f207fedf375bdd
20jonas02 <jogajdosik@gmail.com>
2026-07-19 12:24:48 +0200

    ev refining tweaks, 2.1 hazard recycling reversal

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,5 +1,14 @@
 local data_util = require("__Krastorio2-spaced-out__.data-util")
 
+
+
+
+
+local function add_recipe_category(recipe, category)
+  recipe.categories = recipe.categories or {"crafting"}
+  table.insert(recipe.categories, category)
+end
+
 -- tweak underground belt lengths so you can remove a pair of undergrounds when upgrading
 if settings.startup["krt-belt-length-tweaks"].value then
     data.raw["underground-belt"]["fast-underground-belt"].max_distance = 11
@@ -34,9 +43,9 @@ data.raw["inserter"]["kr-superior-long-inserter"].max_belt_stack_size = 4
 
 -- return of the bio processing unit
 
-if settings.startup["krt-bio-processing-em"].value then data.raw["recipe"]["kr-bio-processing-circuit"].additional_categories = { "electronics" } end
+if settings.startup["krt-bio-processing-em"].value then add_recipe_category(data.raw["recipe"]["kr-bio-processing-circuit"], "electromagnetics") end
 
-data.raw.recipe["kr-ai-core"].additional_categories = {"electronics"}
+add_recipe_category(data.raw.recipe["kr-ai-core"], "electromagnetics")
 
 
 local function change_ingredient(recipe_name, ingredient_name, new_ingredient)
@@ -50,70 +59,53 @@ local function change_ingredient(recipe_name, ingredient_name, new_ingredient)
     end
 
 
-data_util.add_or_replace_ingredient("recycler", "kr-rare-metals", { type = "item", name = "steel-plate", amount = 20 })
+-- data_util.add_or_replace_ingredient("recycler", "kr-rare-metals", { type = "item", name = "steel-plate", amount = 20 })
 data.raw.item["kr-quantum-computer"].default_import_location = "aquilo"
 
-
-
---[[
-local types = {
-	"technology",
-	"recipe",
-	"item",
-	"assembling-machine",
-	"unit",
-	"item-with-entity-data",
-	"capsule",
-	"fish",
-	"turret",
-	"locomotive",
-	"unit-spawner",
-	"corpse",
-	"ammo",
-	"armor",
-	"reactor",
-	"resource",
-	"pipe-to-ground",
-	"splitter",
-	"underground-belt",
-	"transport-belt",
-}
--- restore personal battery mk3
---- @param what Name
-function  unhide(what)
-    for _, type in pairs(types) do
-        local prototype = data.raw[type][what]
-        if prototype then
-            prototype.hidden = false
-            prototype.hidden_in_factoriopedia = false
-        end
-    end
+if mods["sushi-splitters"] then
+    data.raw.recipe["sushi-express-splitter-upgrade"].main_product = nil
 end
 
+data.raw["item"]["tree-seed"].stack_size = 1000
+data.raw["item"]["yumako-seed"].stack_size = 1000
+data.raw["item"]["jellynut-seed"].stack_size = 1000
 
-    unhide("kr-battery-mk3-equipment")
+data.raw["item"]["tree-seed"].weight = 0.1 * kg
+data.raw["item"]["yumako-seed"].weight = 0.1 * kg
+data.raw["item"]["jellynut-seed"].weight = 0.1 * kg
+
+if mods ["ev-refining"] then
     data:extend({
         {
-            type = "technology",
-            name = "kr-battery-mk3-equipment",
-            icon_size = 256,
-            icons = util.technology_icon_constant_equipment("__Krastorio2Assets__/technologies/battery-mk3-equipment.png"),
-            upgrade = false,
-            unit = {
-            count = 500,
-            ingredients = {
-                { "automation-science-pack", 1 },
-                { "logistic-science-pack", 1 },
-                { "chemical-science-pack", 1 },
-                { "production-science-pack", 1 },
+            type = "recipe",
+            name = "rare-metals-ore-alternative-enriching",
+            localised_name = "Molten ore (rare-metals) - Alternative enriching\nEfficiency: 3.0x",
+            icon = "__k2-tweaks__/graphics/icons/rare-metals-ore-alternative-enriching.png",
+            icon_size = 64,
+            categories = {"enriching1"},
+            subgroup = "ev-vulcanus-processes",
+            order = "aa",
+            allow_productivity = true,
+            enabled = false,
+            energy_required = 16,
+            ingredients =
+            {
+                { type = "item", name = "kr-rare-metal-ore", amount = 20 },
+                { type = "item", name = "calcite", amount = 1 }
             },
-            time = 60,
-            },
-            prerequisites = { "kr-quarry-minerals-extraction", "kr-lithium-sulfur-battery", "battery-mk2-equipment" },
-            effects = {
-            { type = "unlock-recipe", recipe = "kr-battery-mk3-equipment" },
-            { type = "unlock-recipe", recipe = "kr-big-battery-mk3-equipment" },
-            },
+            results =
+            {
+                { type = "fluid", name = "kr-molten-rare-metals", amount = 400},
+            }
         }
     })
---]]
+    table.insert(data.raw.technology["elite-ore-processing"].effects, {type = "unlock-recipe", recipe = "rare-metals-ore-alternative-enriching"})
+end
+if mods["god-module"] then
+    data_util.add_or_replace_ingredient("god-module-speed", "kr-ai-core", { type = "item", name = "kr-ai-core", amount = 5 })
+    data_util.add_or_replace_ingredient("god-module-efficiency", "kr-ai-core", { type = "item", name = "kr-ai-core", amount = 5 })
+    data_util.add_or_replace_ingredient("god-module-productivity", "kr-ai-core", { type = "item", name = "kr-ai-core", amount = 5 })
+    data_util.add_or_replace_ingredient("god-module-quality", "kr-ai-core", { type = "item", name = "kr-ai-core", amount = 5 })
+end
+
+table.insert(data.raw["reactor"]["heating-tower"]["energy_source"]["fuel_categories"],"kr-vehicle-fuel")
--- data-updates.lua
+++ data-updates.lua
@@ -21,27 +21,32 @@ if settings.startup["krt-spage-modules"].value then
     change_ingredient_amount("efficiency-module-2", "efficiency-module", 4)
     data.raw.recipe["efficiency-module-3"].energy_required = 60
     change_ingredient_amount("efficiency-module-3", "efficiency-module-2", 4)
-    table.insert(data.raw.recipe["efficiency-module-3"].ingredients, {type = "item", name = "spoilage", amount = 5})
-    table.insert(data.raw.recipe["efficiency-module-3-recycling"].results, {type = "item", name = "spoilage", amount = 1, extra_count_fraction = 0.25})
+    --table.insert(data.raw.recipe["efficiency-module-3"].ingredients, {type = "item", name = "spoilage", amount = 5})
+    --table.insert(data.raw.recipe["efficiency-module-3-recycling"].results, {type = "item", name = "spoilage", amount = 1, extra_count_fraction = 0.25})
 
     data.raw.recipe["quality-module-2"].energy_required = 30
     change_ingredient_amount("quality-module-2", "quality-module", 4)
     data.raw.recipe["quality-module-3"].energy_required = 60
     change_ingredient_amount("quality-module-3", "quality-module-2", 4)
-    table.insert(data.raw.recipe["quality-module-3"].ingredients,{ type = "item", name = "superconductor", amount = 1 })
-    table.insert(data.raw.recipe["quality-module-3-recycling"].results, {type = "item", name = "superconductor", amount = 0, extra_count_fraction = 0.25})
+    --table.insert(data.raw.recipe["quality-module-3"].ingredients,{ type = "item", name = "superconductor", amount = 1 })
+    --table.insert(data.raw.recipe["quality-module-3-recycling"].results, {type = "item", name = "superconductor", amount = 0, extra_count_fraction = 0.25})
 
     data.raw.recipe["productivity-module-2"].energy_required = 30
     change_ingredient_amount("productivity-module-2", "productivity-module", 4)
     data.raw.recipe["productivity-module-3"].energy_required = 60
     change_ingredient_amount("productivity-module-3", "productivity-module-2", 4)
-    table.insert(data.raw.recipe["productivity-module-3"].ingredients,{ type = "item", name = "biter-egg", amount = 1 })
-    table.insert(data.raw.recipe["productivity-module-3-recycling"].results, {type = "item", name = "biter-egg", amount = 0, extra_count_fraction = 0.25})
+    --table.insert(data.raw.recipe["productivity-module-3"].ingredients,{ type = "item", name = "biter-egg", amount = 1 })
+    --table.insert(data.raw.recipe["productivity-module-3-recycling"].results, {type = "item", name = "biter-egg", amount = 0, extra_count_fraction = 0.25})
 
     data.raw.recipe["speed-module-2"].energy_required = 30
     change_ingredient_amount("speed-module-2", "speed-module", 4)
     data.raw.recipe["speed-module-3"].energy_required = 60
     change_ingredient_amount("speed-module-3", "speed-module-2", 4)
-    table.insert(data.raw.recipe["speed-module-3"].ingredients,{ type = "item", name = "tungsten-carbide", amount = 1 })
-    table.insert(data.raw.recipe["speed-module-3-recycling"].results, {type = "item", name = "tungsten-carbide", amount = 0, probaextra_count_fractionbility = 0.25})
+    --table.insert(data.raw.recipe["speed-module-3"].ingredients,{ type = "item", name = "tungsten-carbide", amount = 1 })
+    --table.insert(data.raw.recipe["speed-module-3-recycling"].results, {type = "item", name = "tungsten-carbide", amount = 0, probaextra_count_fractionbility = 0.25})
 end
+if mods["ev-refining"] then
+    data.raw.recipe["coal-chunk-enrichment"].auto_recycle = true
+end
+data.raw.recipe["hazard-concrete"].recycle_to_ingredients_of = nil
+data.raw.recipe["refined-hazard-concrete"].recycle_to_ingredients_of = nil
